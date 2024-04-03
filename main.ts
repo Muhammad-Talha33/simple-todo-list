dc4cf2dfea3e9d82261e68ddb9cbfe015b549aae
Muhammad Talha <mtalha0574758@gmail.com>
2024-04-03 13:18:38 -0700

    "Initialized simple todo-list project"

---
+++ main.ts
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+let todos = [];
+let condition = true;
+
+while (condition) {
+  let addTask = await inquirer.prompt([
+    {
+      name: "todo",
+      type: "input",
+      message: chalk.rgb(
+        255,
+        136,
+        0
+      )("What would you like to add in your todos?"),
+    },
+
+    {
+      name: "addMore",
+      type: "confirm",
+      message: chalk.magenta("Would you like to add more in your todos?"),
+      default: "false",
+    },
+  ]);
+
+  todos.push(addTask.todo);
+  console.log(todos);
+  condition = addTask.addMore;
+}
